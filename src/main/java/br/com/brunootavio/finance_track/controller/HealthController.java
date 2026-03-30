6ca92f8998bc9d115cd24d008218646acabe7019
Bruno Oliveira <brunootavioliveira@gmail.com>
2026-03-30 15:54:20 -0300

    add healthController

--- src/main/java/br/com/brunootavio/finance_track/controller/HealthController.java
+++ src/main/java/br/com/brunootavio/finance_track/controller/HealthController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //é uma anotação do Spring Boot que diz: "Essa classe vai receber requisições HTTP e devolver respostas em formato JSON (normalmente)."
-@RequestMapping("/hello") //endpoint
-public class TestAPI {
+public class HealthController {
 
-    @GetMapping
-    public String hello() {
-        return "API rodando";
+
+    @GetMapping("/")
+    public String health() {
+        return "API running";
     }
 }
